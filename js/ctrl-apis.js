ea8de74731f5464af8d51d6845887628a9481807
José Ventura <stnever@notarealaddress.com>
2013-01-03 21:54:53 -0200

    Models page

--- js/ctrl-apis.js
+++ js/ctrl-apis.js
@@ -21,4 +21,14 @@ function ApiResourcesController( $scope ) {
 }
 
 function ApiModelsController( $scope ) {
+	$scope.api = {
+		"models":{"User":{"id":"User","properties":{"id":{"type":"long"},"lastName":{"type":"string"},"phone":{"type":"string"},"username":{"type":"string"},"email":{"type":"string"},"userStatus":{"allowableValues":{"valueType":"LIST","values":["1-registered","2-active","3-closed"],"valueType":"LIST"},"description":"User Status","type":"int"},"firstName":{"type":"string"},"password":{"type":"string"}}},
+		
+		"Category":{"id":"Category","properties":{"id":{"type":"long"},"name":{"type":"string"}}},
+		
+		"Pet":{"id":"Pet","properties":{"tags":{"items":{"$ref":"Tag"},"type":"Array"},"id":{"type":"long"},"category":{"type":"Category"},"status":{"allowableValues":{"valueType":"LIST","values":["available","pending","sold"],"valueType":"LIST"},"description":"pet status in the store","type":"string"},"name":{"type":"string"},"photoUrls":{"items":{"type":"string"},"type":"Array"}}},
+		
+		"Tag":{"id":"Tag","properties":{"id":{"type":"long"},"name":{"type":"string"}}}
+		}
+	}
 }
